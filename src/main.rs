79dfd53eb07b884e4569c5e921a2a1d3d7e7fe29
clshu <clshu2020@gmail.com>
2023-07-14 22:12:06 -0700

    Add different types of handlers: 1. GET /foo?name=apple&age=23, extractor Query. 2. GET /bar/:user_id, extractor Path. 3. GET /, no extractor. 4. POST /user/login, extractor Json. 5. Return impl IntoResponse. Either plain-text or json.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use axum::{routing::get, Router};
+// #![allow(unused_imports)]
+use axum::extract::{Json, Path, Query};
+use axum::response::IntoResponse;
+use axum::{routing::get, routing::post, Router};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -9,19 +14,52 @@ async fn main() {
 
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/bar", get(foo_bar));
+        .route("/foo", get(get_foo))
+        .route("/bar/:user_id", get(get_bar))
+        .route("/user/login", post(login))
+        .route("/", get(root));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
 
-    // which calls one of these handlers
-    async fn root() {}
-    async fn get_foo() {}
-    async fn post_foo() {}
-    async fn foo_bar() {}
+async fn root() -> impl IntoResponse {
+    "Hello, World!"
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct UserParams {
+    name: String,
+    age: u8,
+}
+
+// `Query` gives you the query parameters and deserializes them.
+// async fn query(Query(params): Query<HashMap<String, String>>) {}
+async fn get_foo(Query(params): Query<UserParams>) -> impl IntoResponse {
+    Json(json!(params))
+}
+
+// `Path` gives you the path parameters and deserializes them.
+// async fn path(Path(user_id): Path<u32>) {}
+
+async fn get_bar(Path(user_size): Path<u32>) -> impl IntoResponse {
+    Json(json!({ "user_size": user_size }))
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct LoginParams {
+    username: String,
+    password: String,
+}
+
+// Buffer the request body and deserialize it as JSON into a
+// `serde_json::Value`. `Json` supports any type that implements
+// `serde::Deserialize`.
+// async fn json(Json(payload): Json<serde_json::Value>) {}
+
+async fn login(Json(params): Json<LoginParams>) -> impl IntoResponse {
+    Json(json!(params))
 }
