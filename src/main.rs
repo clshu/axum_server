4e80a32f689ac9c8e7d557212e5a41b6fdb218f5
clshu <clshu2020@gmail.com>
2023-07-14 20:07:24 -0700

    Add dummy handlers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use axum::{routing::get, Router};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/bar", get(foo_bar));
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    // which calls one of these handlers
+    async fn root() {}
+    async fn get_foo() {}
+    async fn post_foo() {}
+    async fn foo_bar() {}
 }
