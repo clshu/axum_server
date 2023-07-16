25a665491f7756d628d59cdbe9acc0f3f2330083
clshu <clshu2020@gmail.com>
2023-07-16 01:31:15 -0700

    Remove dummy handlers. Refactor graphql handler to a single file.

---
+++ src/graphql/handlers.rs
@@ -0,0 +1,19 @@
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    extract::Extension,
+    response::{self, IntoResponse},
+};
+
+use crate::graphql::greeting::RootQuery;
+
+pub async fn graphql_endpoint(
+    schema: Extension<Schema<RootQuery, EmptyMutation, EmptySubscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+pub async fn graphql_playground() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 // #![allow(unused_imports)]
 mod graphql;
 mod utils;
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{
-    extract::{Extension, Json, Path, Query},
-    response::{self, IntoResponse},
-    routing::{get, post},
-    Router,
-};
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+
+use axum::{extract::Extension, routing::get, Router};
+
 use std::net::SocketAddr;
 
-use graphql::greeting::RootQuery;
+use graphql::{
+    greeting::RootQuery,
+    handlers::{graphql_endpoint, graphql_playground},
+};
 use utils::constants::GRAPHQL_PATH;
 
 #[tokio::main]
@@ -26,10 +23,6 @@ async fn main() {
 
     // build our application with a single route
     let app = Router::new()
-        .route("/foo", get(get_foo))
-        .route("/bar/:user_id", get(get_bar))
-        .route("/user/login", post(login))
-        .route("/", get(root))
         .route(GRAPHQL_PATH, get(graphql_playground).post(graphql_endpoint))
         .layer(Extension(schema));
 
@@ -38,53 +31,4 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-async fn root() -> impl IntoResponse {
-    "Hello, World!"
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct UserParams {
-    name: String,
-    age: u8,
-}
-
-// `Query` gives you the query parameters and deserializes them.
-// async fn query(Query(params): Query<HashMap<String, String>>) {}
-async fn get_foo(Query(params): Query<UserParams>) -> impl IntoResponse {
-    Json(json!(params))
-}
-
-// `Path` gives you the path parameters and deserializes them.
-// async fn path(Path(user_id): Path<u32>) {}
-
-async fn get_bar(Path(user_size): Path<u32>) -> impl IntoResponse {
-    Json(json!({ "user_size": user_size }))
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct LoginParams {
-    username: String,
-    password: String,
-}
-
-// Buffer the request body and deserialize it as JSON into a
-// `serde_json::Value`. `Json` supports any type that implements
-// `serde::Deserialize`.
-// async fn json(Json(payload): Json<serde_json::Value>) {}
-
-async fn login(Json(params): Json<LoginParams>) -> impl IntoResponse {
-    Json(json!(params))
-}
-
-async fn graphql_endpoint(
-    schema: Extension<Schema<RootQuery, EmptyMutation, EmptySubscription>>,
-    req: GraphQLRequest,
-) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
-}
-
-async fn graphql_playground() -> impl IntoResponse {
-    response::Html(GraphiQLSource::build().endpoint("/").finish())
 }
