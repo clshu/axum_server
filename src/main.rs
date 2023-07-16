0ed061bf8bca0e5980af0f565b9303dbb9d5fedb
clshu <clshu2020@gmail.com>
2023-07-16 02:03:24 -0700

    Add a fallback service to load public/index.html

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod utils;
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
 
 use axum::{extract::Extension, routing::get, Router};
+use tower_http::services::ServeFile;
 
 use std::net::SocketAddr;
 
@@ -17,6 +18,8 @@ use utils::constants::GRAPHQL_PATH;
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let serve_file = ServeFile::new("./public/index.html");
+
     let schema = Schema::build(RootQuery, EmptyMutation, EmptySubscription).finish();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -24,7 +27,8 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route(GRAPHQL_PATH, get(graphql_playground).post(graphql_endpoint))
-        .layer(Extension(schema));
+        .layer(Extension(schema))
+        .fallback_service(serve_file);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&addr)
